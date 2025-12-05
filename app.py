559e7fa84e7d158d753eba468da82bf23231117d
JacopoDev1234 <rocchi.jacopo97@gmail.com>
2025-12-05 14:11:16 +0100

    commento tottigol17

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import datetime
 
 app = Flask(__name__)
 
-# --- CONFIG BASE ---
+# --- CONFIG BASE --- tottigol
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = "cambia-questa-chiave-super-segreta"
